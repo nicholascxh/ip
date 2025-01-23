3108c718dab13e7edd36ff315026b085519c499d
nicholascxh <02nicholaschong@gmail.com>
2025-01-24 01:34:59 +0800

    Renamed chatbot from Duke to Dasani with basic greeting and exit functionality

---
+++ src/main/java/Dasani.java
@@ -0,0 +1,13 @@
+public class Dasani {
+    public static void main(String[] args) {
+        // Greeting message
+        System.out.println("____________________________________________________________");
+        System.out.println(" Hello! I'm Dasani"); // Replace "Nova" with your chatbot name
+        System.out.println(" What can I do for you?");
+        System.out.println("____________________________________________________________");
+
+        // Exit message
+        System.out.println(" Bye. Hope to see you again soon!");
+        System.out.println("____________________________________________________________");
+    }
+}
